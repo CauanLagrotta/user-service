a56ce4f4695c224fcbb702085ff0051a58ba67f7
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-06-03 21:24:35 -0300

    feat: add JPA entity and repository for User

---
+++ src/main/java/com/cauanlagrotta/model/User.java
@@ -0,0 +1,102 @@
+package com.cauanlagrotta.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String fullName;
+  private String email;
+  private String phone;
+  private String role;
+  private String password;
+
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+
+  public User(String fullName, String email, String phone, String role, String password, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    this.fullName = fullName;
+    this.email = email;
+    this.phone = phone;
+    this.role = role;
+    this.password = password;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  public User() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+}
