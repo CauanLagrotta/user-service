e35a2a74d014404104ebafa96a617cd8d4332d5b
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-06-05 20:28:35 -0300

    feat: add basic crud operations for users controller

--- src/main/java/com/cauanlagrotta/controller/UserController.java
+++ src/main/java/com/cauanlagrotta/controller/UserController.java
@@ -1,14 +1,66 @@
 package com.cauanlagrotta.controller;
 
 import com.cauanlagrotta.model.User;
-import org.springframework.web.bind.annotation.RestController;
+import com.cauanlagrotta.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
 
-  public User getUser() {
-    User user = new User();
+  private final UserRepository userRepository;
 
-    return user;
+  public UserController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @PostMapping("/api/users")
+  public User createUser(@RequestBody User user){
+    return userRepository.save(user);
+  }
+
+  @GetMapping("/api/users")
+  public List<User> getUsers() {
+    return userRepository.findAll();
+  }
+
+  @GetMapping("/api/users/{id}")
+  public User getUserById(@PathVariable("id") Long id){
+    Optional<User> user = userRepository.findById(id);
+    return user.orElseThrow(() -> new RuntimeException("User not found"));
+  }
+
+  @PutMapping("/api/users/{id}")
+  public User updateUser(@PathVariable Long id,
+                         @RequestBody User user) {
+
+    Optional<User> opt = userRepository.findById(id);
+    if (opt.isEmpty()) {
+      throw new RuntimeException("User not found");
+    }
+
+    User existingUser = opt.get();
+
+    existingUser.setFullName(user.getFullName());
+    existingUser.setEmail(user.getEmail());
+    existingUser.setPhone(user.getPhone());
+    existingUser.setRole(user.getRole());
+    existingUser.setPassword(user.getPassword());
+    existingUser.setUpdatedAt(LocalDateTime.now());
+
+    return userRepository.save(existingUser);
+
+  }
+
+  @DeleteMapping("/api/users/{id}")
+  public void deleteUser(@PathVariable("id") Long id) {
+    Optional<User> opt = userRepository.findById(id);
+    if (opt.isEmpty()) {
+      throw new RuntimeException("User not found");
+    }
+    userRepository.deleteById(id);
   }
 }
